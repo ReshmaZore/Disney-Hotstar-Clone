a217f017465353dcaad0ffc9061a9d9b4c6aab22
ReshmaZore <zorereshma095@gmail.com>
2025-02-03 09:30:52 +0530

    second commit

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -5,10 +5,10 @@ const api_key='302b4eedc19c9c9ad154d28e3e8d28dd'
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=302b4eedc19c9c9ad154d28e3e8d28dd
 
-const getTrendingVideos=axios.get(movieBaseUrl+
+const getTrendingVideos=axios.get(MovieBaseUrl+
     "/trending/all/day?api_key="+api_key);
     const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+    axios.get(MovieByGenreBaseURL+"&with_genres="+id)
 
 export default{
     getTrendingVideos,
