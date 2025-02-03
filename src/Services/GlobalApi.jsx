aaabe7c80b26d287ea46796660de34fe0a71ae71
ReshmaZore <zorereshma095@gmail.com>
2025-02-04 01:21:13 +0530

    completed

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 const MovieBaseUrl = "https://api.themoviedb.org/3"
 const api_key='302b4eedc19c9c9ad154d28e3e8d28dd'
 
+const MovieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=302b4eedc19c9c9ad154d28e3e8d28dd';
+
 // https://api.themoviedb.org/3/trending/all/day?api_key=302b4eedc19c9c9ad154d28e3e8d28dd
 
 const getTrendingVideos=axios.get(MovieBaseUrl+
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 function HeaderItem({name , Icon}) {
   return (
